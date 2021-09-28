408807756956120e8755ce8be0acf41e8f70b2a4
ta-danny-r <riverad@codingdojo.com>
2021-09-28 02:29:56 -0700

    Intro to Storyboard

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+// View Controller is used to manage UI components
 class ViewController: UIViewController {
-
+    
+    // We created a reference of these UI Elements from Storyboard
+    // IBOutlet stands for Interface Builder Outlet
+    @IBOutlet var changeLabel: UILabel!
+    @IBOutlet var button: UIButton!
+    
+    // viewDidLoad is part of the View Controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // We have to create an event for a certain action.
+    // Creates a function to handle an action when button is click
+    // IBAction stands for Interface Builder Action
+    @IBAction func onButtonClick(_ sender: UIButton) {
+        changeLabel.text = "Button has been clicked"
+    }
+    
+    
 }
 
